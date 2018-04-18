f420d8d6805f97c5c01f956a67aa60a8cba4fe7a
asromeroc <32311777+asromeroc@users.noreply.github.com>
2018-04-17 21:37:55 -0300

    primera version
    primera version belife

---
+++ Aplicacion.Datos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Aplicacion.Datos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aplicacion.Datos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("98fad484-cf75-4e36-a0f9-0823ce72ca07")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
+// mediante el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Aplicacion.Presentacion/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aplicacion.Datos;
+namespace Aplicacion.Presentacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BeLifeEntities conector;
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (conector == null)
+            {
+                conector = new BeLifeEntities();
+            }
+            ActualizarLista();
+        }
+
+        private void ActualizarLista()
+        {
+            dgListarClientes.ItemsSource = null;
+            dgListarClientes.ItemsSource = conector.Clientes.ToList();
+
+        }
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            int idSexo = 0;
+            int idEstadoCivil = 0;
+            //string edad;
+           
+                //Valida nombres
+                if (string.IsNullOrEmpty(txtNombres.Text))
+                {
+
+                    MessageBox.Show("Debe ingresar los nombres");
+                    txtNombres.Focus();
+
+                    return;
+
+                }
+                //valida apellidos
+                if (string.IsNullOrEmpty(txtApellidos.Text))
+                {
+
+                    MessageBox.Show("Debe ingresar los apellidos");
+                    txtApellidos.Focus();
+                    return;
+
+                }
+                //valida rut
+                if (string.IsNullOrEmpty(txtRut.Text))
+                {
+
+                    MessageBox.Show("Debe el rut");
+                    txtRut.Focus();
+                    return;
+
+                }
+                //Valida sexo
+                if (cbSexo.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Debe seleccionar sexo");
+                    cbSexo.Focus();
+                }
+                else
+                {
+
+                    if (cbSexo.SelectedIndex == 0)
+
+                    {
+                        //id sexo = hombre
+                        idSexo = 1;
+                    }
+                    else
+                    {
+                        //id sexo = mujer
+                        idSexo = 2;
+                    }
+                }
+                
+                //valida estado civil
+                if (cbEstadoCivil.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Debe seleccionar Estado Civil");
+                    cbEstadoCivil.Focus();
+                }
+                else
+                {
+
+                    if (cbSexo.SelectedIndex == 0)
+
+                    {
+                        //soltero
+                        idSexo = 1;
+                    }
+                    if (cbSexo.SelectedIndex == 1)
+                    {
+                        //casado
+                        idSexo = 2;
+                    }
+                    if (cbSexo.SelectedIndex == 2)
+                    {
+                        //divorciado
+                        idSexo = 3;
+                    }
+                    else
+                    {
+                        //viudo
+                        idSexo = 4;
+                    }
+
+                }
+
+            //    //valida edad
+            //    edad = dpInsertarFechaNacimiento.SelectedDate.ToString();
+                
+
+            //if (edad < 0)
+            //    {
+            //       MessageBox.Show("la fecha seleccionada es igual o mayor al dia actual");
+            //    }
+
+            //    int años = edad / 365;
+
+            //    if (años < 18)
+            //    {
+            //    MessageBox.Show("debe ser mayor a 18 años");
+            //    }
+
+
+
+
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Nombres = txtNombres.Text;
+                cli.Apellidos = txtApellidos.Text;
+                cli.FechaNacimiento = dpInsertarFechaNacimiento.SelectedDate.ToString();
+                cli.IdSexo = idSexo;
+                cli.RutCliente = txtRut.Text;
+                cli.IdEstadoCivil = idEstadoCivil;
+
+
+                conector.Clientes.Add(cli);
+                conector.SaveChanges();
+
+                MessageBox.Show("Se ha Ingresado un cliente");
+                txtNombres.Text = string.Empty;
+                txtRut.Text = string.Empty;
+                txtApellidos.Text = string.Empty;
+                ActualizarLista();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
